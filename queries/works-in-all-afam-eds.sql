49cb8cf92ded88b334b7b66b8c6bf7fdd0fd4a74
Erik Fredner <erik.fredner@oregonstate.edu>
2026-05-04 09:49:35 -0400

    Create works-in-all-afam-eds.sql

---
+++ queries/works-in-all-afam-eds.sql
@@ -0,0 +1,25 @@
+SELECT w.id,
+       w.title
+FROM data_work AS w
+JOIN data_workinanthology AS wia
+  ON wia.work_id = w.id
+JOIN data_volume AS v
+  ON v.id = wia.volume_id
+JOIN data_edition AS e
+  ON e.id = v.edition_id
+JOIN data_edition_literary_traditions AS elt
+  ON elt.edition_id = e.id
+JOIN data_literarytradition AS lt
+  ON lt.id = elt.literarytradition_id
+WHERE lt."name" = 'African-American Literature'
+GROUP BY w.id, w.title
+HAVING COUNT(DISTINCT e.id) = (
+    SELECT COUNT(DISTINCT e2.id)
+    FROM data_edition AS e2
+    JOIN data_edition_literary_traditions AS elt2
+      ON elt2.edition_id = e2.id
+    JOIN data_literarytradition AS lt2
+      ON lt2.id = elt2.literarytradition_id
+    WHERE lt2."name" = 'African-American Literature'
+)
+ORDER BY w.title;
